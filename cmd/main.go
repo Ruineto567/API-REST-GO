e514b3603134e96e9ac0daf8c9cfde8ffa08ffec
Ruineto567 <netoruimedeiros@gmail.com>
2026-06-23 21:07:01 -0300

    Iniciando aplicacao

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	//Camada de Repository
+	productRepository := repository.NewProductRepository(dbConnection)
+	//Camada de UseCase
+	ProductUseCase := usecase.NewProductUsecase(productRepository)
+	//Camada de Controller
+	productController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", productController.GetProducts)
+
+	server.Run(":3333")
+}
